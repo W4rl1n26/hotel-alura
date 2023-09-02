53b8c4ba0182797490ab47a0befb3ef24fed13fe
W4rl1n26 <129914960+W4rl1n26@users.noreply.github.com>
2023-09-02 16:19:41 -0600

    Codigo base

---
+++ src/main/java/com/alura/main/HotelAluraNetbeans.java
@@ -0,0 +1,13 @@
+
+
+package com.alura.main;
+
+import com.alura.views.Login;
+
+public class HotelAluraNetbeans {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
