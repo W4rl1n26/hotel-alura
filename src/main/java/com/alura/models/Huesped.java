128b0e518a110ff4cd399f16a5e444b9818db13b
W4rl1n26 <129914960+W4rl1n26@users.noreply.github.com>
2023-09-02 16:36:43 -0600

    Creación de clase modelo huesped

---
+++ src/main/java/com/alura/models/Huesped.java
@@ -0,0 +1,84 @@
+
+package com.alura.models;
+
+import java.time.LocalDate;
+
+public class Huesped {
+    
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    private String nacionalidad;
+    private String telefono;
+    
+    public Huesped() {}
+    
+    public Huesped (String nombre, String apellido, LocalDate fechaNacimiento, String nacionalidad, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nacionalidad = nacionalidad;
+        this.telefono = telefono;
+    }
+
+    public Huesped (Integer id, String nombre, String apellido, LocalDate fechaNacimiento, String nacionalidad, String telefono) {
+        
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nacionalidad = nacionalidad;
+        this.telefono = telefono;
+        
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
